661d17650e144b8f55f8964c9efd3f0149ce61fd
Clarence Mollet <clarencemollet1@gmail.com>
2024-12-03 11:34:48 +0100

    [add] Main CPP and Blueprint class + Inputs

---
+++ Source/UE_Traversal/Public/HUD_Main.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "HUD_Main.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_TRAVERSAL_API AHUD_Main : public AHUD
+{
+	GENERATED_BODY()
+	
+};
